6edd594cf61afeec679bfed232920704ec365ec2
Avihay Shem Tov <1avihay@gmail.com>
2015-04-09 02:45:26 +0300

    Some UI changes

--- IronManConsole/Program.cs
+++ IronManConsole/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace IronManConsole
 {
@@ -11,9 +13,15 @@ namespace IronManConsole
     {
         static void Main(string[] args)
         {
-            new App().run();
-            Console.Read();
-            
+            //new App().run();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Main a = new Main();
+            a.Load += delegate
+            {
+                a.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - a.Width, Screen.PrimaryScreen.WorkingArea.Height - a.Height);
+            };
+            Application.Run(a);
         }
     }
 }
